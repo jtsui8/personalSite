8985b89251b1ee1b986a5e99cebf44bc14d9a31f
Johnny Tsui <jtsui810@gmail.com>
2023-12-21 19:46:22 -0800

    adding header and footer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+// import App from './App.tsx';
 // import './index.css'
+
+import Header from './Header.tsx';
+import Footer from './Footer.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<Header />
+		<Footer />
 	</React.StrictMode>
 );
