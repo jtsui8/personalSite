68f53983e6377dff12264d906004a560ff7729d2
Johnny Tsui <jtsui810@gmail.com>
2024-01-14 11:23:15 -0800

    redo layout. changing grid

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,72 @@ function App() {
 					</p>
 				</div>
 				<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in eros in elit sodales aliquam sed
+					sed elit. In hac habitasse platea dictumst. Integer mollis consequat ex ac commodo. Nunc viverra
+					tincidunt nisi sed mattis. Maecenas sapien mauris, cursus ac commodo non, volutpat in lectus. Donec
+					tristique porttitor velit non lobortis. Curabitur augue dolor, ultrices eu imperdiet non, eleifend
+					sit amet nisl. Quisque volutpat eros interdum ex dignissim, tempus tempus lorem scelerisque. Vivamus
+					sodales elementum tortor non porttitor. Sed ac sapien orci. Nullam pellentesque ligula vitae tellus
+					aliquet tincidunt. Proin facilisis augue eget dignissim tincidunt. Proin commodo neque ac quam
+					vulputate, at tristique urna hendrerit. Nulla facilisi. Donec feugiat sollicitudin magna et
+					sagittis. Mauris pharetra posuere porta. Donec quis velit porta, scelerisque mi at, facilisis mi.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in eros in elit sodales aliquam sed
+					sed elit. In hac habitasse platea dictumst. Integer mollis consequat ex ac commodo. Nunc viverra
+					tincidunt nisi sed mattis. Maecenas sapien mauris, cursus ac commodo non, volutpat in lectus. Donec
+					tristique porttitor velit non lobortis. Curabitur augue dolor, ultrices eu imperdiet non, eleifend
+					sit amet nisl. Quisque volutpat eros interdum ex dignissim, tempus tempus lorem scelerisque. Vivamus
+					sodales elementum tortor non porttitor. Sed ac sapien orci. Nullam pellentesque ligula vitae tellus
+					aliquet tincidunt. Proin facilisis augue eget dignissim tincidunt. Proin commodo neque ac quam
+					vulputate, at tristique urna hendrerit. Nulla facilisi. Donec feugiat sollicitudin magna et
+					sagittis. Mauris pharetra posuere porta. Donec quis velit porta, scelerisque mi at, facilisis mi.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in eros in elit sodales aliquam sed
+					sed elit. In hac habitasse platea dictumst. Integer mollis consequat ex ac commodo. Nunc viverra
+					tincidunt nisi sed mattis. Maecenas sapien mauris, cursus ac commodo non, volutpat in lectus. Donec
+					tristique porttitor velit non lobortis. Curabitur augue dolor, ultrices eu imperdiet non, eleifend
+					sit amet nisl. Quisque volutpat eros interdum ex dignissim, tempus tempus lorem scelerisque. Vivamus
+					sodales elementum tortor non porttitor. Sed ac sapien orci. Nullam pellentesque ligula vitae tellus
+					aliquet tincidunt. Proin facilisis augue eget dignissim tincidunt. Proin commodo neque ac quam
+					vulputate, at tristique urna hendrerit. Nulla facilisi. Donec feugiat sollicitudin magna et
+					sagittis. Mauris pharetra posuere porta. Donec quis velit porta, scelerisque mi at, facilisis mi.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in eros in elit sodales aliquam sed
+					sed elit. In hac habitasse platea dictumst. Integer mollis consequat ex ac commodo. Nunc viverra
+					tincidunt nisi sed mattis. Maecenas sapien mauris, cursus ac commodo non, volutpat in lectus. Donec
+					tristique porttitor velit non lobortis. Curabitur augue dolor, ultrices eu imperdiet non, eleifend
+					sit amet nisl. Quisque volutpat eros interdum ex dignissim, tempus tempus lorem scelerisque. Vivamus
+					sodales elementum tortor non porttitor. Sed ac sapien orci. Nullam pellentesque ligula vitae tellus
+					aliquet tincidunt. Proin facilisis augue eget dignissim tincidunt. Proin commodo neque ac quam
+					vulputate, at tristique urna hendrerit. Nulla facilisi. Donec feugiat sollicitudin magna et
+					sagittis. Mauris pharetra posuere porta. Donec quis velit porta, scelerisque mi at, facilisis mi.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in eros in elit sodales aliquam sed
+					sed elit. In hac habitasse platea dictumst. Integer mollis consequat ex ac commodo. Nunc viverra
+					tincidunt nisi sed mattis. Maecenas sapien mauris, cursus ac commodo non, volutpat in lectus. Donec
+					tristique porttitor velit non lobortis. Curabitur augue dolor, ultrices eu imperdiet non, eleifend
+					sit amet nisl. Quisque volutpat eros interdum ex dignissim, tempus tempus lorem scelerisque. Vivamus
+					sodales elementum tortor non porttitor. Sed ac sapien orci. Nullam pellentesque ligula vitae tellus
+					aliquet tincidunt. Proin facilisis augue eget dignissim tincidunt. Proin commodo neque ac quam
+					vulputate, at tristique urna hendrerit. Nulla facilisi. Donec feugiat sollicitudin magna et
+					sagittis. Mauris pharetra posuere porta. Donec quis velit porta, scelerisque mi at, facilisis mi.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in eros in elit sodales aliquam sed
+					sed elit. In hac habitasse platea dictumst. Integer mollis consequat ex ac commodo. Nunc viverra
+					tincidunt nisi sed mattis. Maecenas sapien mauris, cursus ac commodo non, volutpat in lectus. Donec
+					tristique porttitor velit non lobortis. Curabitur augue dolor, ultrices eu imperdiet non, eleifend
+					sit amet nisl. Quisque volutpat eros interdum ex dignissim, tempus tempus lorem scelerisque. Vivamus
+					sodales elementum tortor non porttitor. Sed ac sapien orci. Nullam pellentesque ligula vitae tellus
+					aliquet tincidunt. Proin facilisis augue eget dignissim tincidunt. Proin commodo neque ac quam
+					vulputate, at tristique urna hendrerit. Nulla facilisi. Donec feugiat sollicitudin magna et
+					sagittis. Mauris pharetra posuere porta. Donec quis velit porta, scelerisque mi at, facilisis mi.
+				</p>
 			</section>
 		</>
 	);
